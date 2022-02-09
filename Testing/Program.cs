af8579e2cbfb8b6f22fb5f4f88b2f48485f58980
bozinandrey1@yandex.ru <bozinandrey1@yandex.ru>
2022-02-09 14:13:27 +0300

    First prak

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -6,12 +6,21 @@ namespace Testing
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите a:");
             double a = double.Parse(Console.ReadLine());
+            Console.Write("Введите b:");
             double b = double.Parse(Console.ReadLine());
+            Console.Write("Введите c:");
             double c = double.Parse(Console.ReadLine());
+            while (c == 0)
+            {
+                Console.Write("Ты инвалид\nВведите c:");
+                c = double.Parse(Console.ReadLine());
+            }
+            Console.Write("Введите x:");
             double x = double.Parse(Console.ReadLine());
 
-            double y = (c < 0 || b != 0) ? a * Math.Pow(x, 2) + Math.Pow(b, 2) * x : (c > 0 && b == 0) ? (x + a)/(x + c) : x/c;
+            double y = (c > 0 && b == 0) ? (x + a) / (x + c) : (c < 0 || b != 0) ? a * Math.Pow(x, 2) + Math.Pow(b, 2) * x : x/c;
 
             Console.WriteLine(y);
         }
