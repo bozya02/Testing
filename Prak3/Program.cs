03f05f3d8e1df375a007dd282fbde2f8becd17d7
201913 <bozinandrey1@yandex.ru>
2022-03-12 10:39:11 +0300

    Prak3

---
+++ Prak3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Prak3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double a = 0;
+            double b = 2;
+            double e;
+            try
+            {
+                e = double.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Entered value is not number");
+                return;
+            }
+            Console.WriteLine(HalfDivision(a, b, e));
+        }
+        public static double HalfDivision(double a, double b, double e)
+        {
+            while (b - a > 2 * e)
+            {
+                double c = (a + b) / 2;
+
+                if (Calc(a) * Calc(c) < 0)
+                {
+                    b = c;
+                }
+                else
+                {
+                    a = c;
+                }
+            }
+            return (a + b) / 2;
+        }
+
+        public static double Calc(double x)
+        {
+            return x + Math.Log(x + 0.5) - 0.5;
+        }
+    }
+}
