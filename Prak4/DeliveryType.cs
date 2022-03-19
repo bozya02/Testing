9a8dff695864b82b87eb0131eb7872a4e39f3093
201913 <bozinandrey1@yandex.ru>
2022-03-19 10:11:33 +0300

    Start Prak4

---
+++ Prak4/DeliveryType.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Prak4
+{
+    public class DeliveryType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Prak4/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Prak4
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int BuyerId { get; set; }
+        public int ProductId { get; set; }
+        public int count { get; set; }
+        public int DeliveryTypeId { get; set; }
+    }
+}
