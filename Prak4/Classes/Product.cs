089d8a2b3b7657b14213c0ad54a934a297fe2e13
Михаил Мясников <gutufik@mail.ru>
2022-03-23 14:43:57 +0300

    Almost done

--- Prak4/Classes/Product.cs
+++ Prak4/Classes/Product.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Prak4.Classes
 {
     public class Product
     {
+        [JsonProperty("id")]
         public int Id { get; set; }
+        [JsonProperty("name")]
         public string Name { get; set; }
+        [JsonProperty("description")]
         public string Description { get; set; }
+        [JsonProperty("producer")]
         public string Producer { get; set; }
+        [JsonProperty("price")]
         public double Price { get; set; }
-        public bool IsActive { get; set; }
+        [JsonProperty("active")]
+        public string Active { get; set; }
+        public bool IsActive { get { return Active != "0"; }  }
+        [JsonProperty("count")]
         public int Count { get; set; }
     }
 }
--- Prak4/MainWindow.xaml.cs
+++ Prak4/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Prak4.Classes;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace Prak4
 {
@@ -20,9 +23,50 @@ namespace Prak4
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Product> Products { get; set; }
+        public List<string> Producers { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            Products = JsonConvert.DeserializeObject<List<Product>>(File.ReadAllText("..\\..\\Toys.json"));
+
+            Producers = new List<string>();
+            foreach (var prod in Products)
+            {
+                if (!Producers.Contains(prod.Producer))
+                    Producers.Add(prod.Producer);
+            }
+            Producers.Add("Все");
+
+            DataContext = this;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var result = new List<Product>(Products);
+            try
+            {
+                if (cbProducer.SelectedItem as string == "Все")
+                    throw new Exception();
+
+                result = result.Where(x => x.Producer == cbProducer.SelectedItem as string).ToList();
+            }
+            catch { }
+            phonesGrid.ItemsSource = result;
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            var result = new List<Product>(Products);
+            if (serchBox.Text != "")
+                result = result.Where(x => x.Name.Contains(serchBox.Text) || x.Description.Contains(serchBox.Text)).ToList();
+
+            phonesGrid.ItemsSource = result;
+        }
+
+        private void Buy_Click(object sender, RoutedEventArgs e)
+        {
+            (new BuyWindow()).Show();
         }
     }
 }
