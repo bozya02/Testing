d7982d04fbbc88b9cee717de0c45067a94fcecfe
Михаил Мясников <gutufik@mail.ru>
2022-04-25 14:19:16 +0300

    Added simple calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        public static Dictionary<string, Func<double, double, double>> Operators = new Dictionary<string, Func<double, double, double>>()
+        {
+            {"+", Sum },
+            {"-", Substract},
+            {"*", Multiple },
+            {"/", Divide }
+        };
+        static void Main(string[] args)
+        {
+            try
+            {
+                var input = Console.ReadLine().Split(' ');
+                var a = Convert.ToDouble(input[0]);
+                var b = Convert.ToDouble(input[2]);
+                var operation = input[1];
+                Console.WriteLine(Operators[operation](a, b));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+        public static double Divide(double a, double b)
+        {
+            return a / b;
+        }
+        public static double Multiple(double a, double b)
+        {
+            return a * b;
+        }
+        public static double Substract(double a, double b)
+        {
+            return a - b;
+        }
+    }
+}
