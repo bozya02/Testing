8405d1259a73396c677846812461911d02db5378
bozinandrey1@yandex.ru <bozinandrey1@yandex.ru>
2022-04-25 15:02:43 +0300

    Complete Prak5

---
+++ Prak5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prak5
+{
+    public class Program
+    {
+        static Dictionary<string, Func<double, double, double>> operators = new Dictionary<string, Func<double, double, double>>
+        {
+            { "+", Sum },
+            { "-",  Subtraction},
+            { "*",  Multiplication},
+            { "/",  Division}
+        };
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите a: ");
+                double a = Double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите b: ");
+                double b = Double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите оператор (+, - , *, /): ");
+                string oper = Console.ReadLine();
+
+                Console.WriteLine(operators[oper](a, b));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public static double Sum(double a, double b) => a + b;
+        public static double Subtraction(double a, double b) => a - b;
+        public static double Multiplication(double a, double b) => a * b;
+        public static double Division(double a, double b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException();
+            return a / b;
+        }
+    }
+}
---
+++ Prak5Test/UnitTestCalc.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+using Microsoft.Analytics.UnitTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Prak5;
+
+namespace Prak5Test
+{
+    [TestClass]
+    public class UnitTestCalc
+    {
+        [TestMethod]
+        public void SumTest()
+        {
+            double a = 5;
+            double b = 23;
+
+            var expected = a + b;
+
+            Assert.AreEqual(expected, Program.Sum(a, b));
+        }
+
+        [TestMethod]
+        public void SubtractionTest()
+        {
+            double a = 5;
+            double b = 23;
+
+            var expected = a - b;
+
+            Assert.AreEqual(expected, Program.Subtraction(a, b));
+        }
+
+        [TestMethod]
+        public void MultiplicationTest()
+        {
+            double a = 5;
+            double b = 23;
+
+            var expected = a * b;
+
+            Assert.AreEqual(expected, Program.Multiplication(a, b));
+        }
+
+        [TestMethod]
+        public void DivisionTest()
+        {
+            double a = 5;
+            double b = 23;
+
+            var expected = a / b;
+
+            Assert.AreEqual(expected, Program.Division(a, b));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void DivideByZeroExceprion()
+        {
+            double a = 5;
+            double b = 0;
+
+            var expected = a / b;
+
+            Assert.AreEqual(expected, Program.Division(a, b));
+        }
+    }
+}
